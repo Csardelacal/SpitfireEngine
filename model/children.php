47345506947fae9f685185197d105f8f14ff0c09
César de la Cal <flunsidelacal@msn.com>
2016-09-05 16:05:56 +0200

    The childrenField class now has the old methods deprecated. Added some documentation too.

--- model/children.php
+++ model/children.php
@@ -3,7 +3,15 @@
 use spitfire\model\Field;
 use spitfire\Model;
 use spitfire\model\adapters\ChildrenAdapter;
+use spitfire\exceptions\PrivateException;
 
+/**
+ * The children field allows the application to maintain a relationship in the 
+ * database that acts like an array of models that depend on the current model.
+ * 
+ * @todo Move to a proper namespaced class, but we need to find a mechanism for
+ * aliasing classes that works properly.
+ */
 class ChildrenField extends Field
 {
 	private $role;
@@ -33,14 +41,39 @@ class ChildrenField extends Field
 		}
 	}
 	
+	/**
+	 * Retrieves the field that this child field references. Although the children
+	 * field has no representation in the DBMS it does provide a simplified 
+	 * mechanism to access the data in a table that references the current model.
+	 * 
+	 * If the programmer provided a target we can just check that the field exists
+	 * and return it, otherwise we need to search for a field that references
+	 * this model and return that.
+	 * 
+	 * @return \Reference
+	 * @throws PrivateException
+	 */
 	public function getReferencedField() {
 		if (!empty($this->role)) {
+			#If a role is predefined, we already know what to get
 			return $this->getTarget()->getField($this->role);
 		} else {
 			$fields = $this->getTarget()->getFields();
-			foreach ($fields as $field)
-				if ($field instanceof \Reference && $field->getTarget() === $this)
-					return $field;
+			
+			#Since we could have several items pointing at our Schema we will be
+			#filtering the remote fields looking for candidates.
+			$candidates = array_filter($fields, function ($f) {
+				return $f instanceof \Reference && $f->getTarget() === $this;
+			});
+			
+			#If there were no candidates we need to let the programmer know
+			if (empty($candidates)) {
+				throw new PrivateException('Children field pointing at a model that does not reference it back.');
+			}
+			
+			#Once we have the candidates we return the first we found, since no other
+			#option was provided.
+			return reset($candidates);
 		}
 	}
 	
@@ -51,11 +84,23 @@ class ChildrenField extends Field
 	public function getPhysical() {
 		return Array();
 	}
-
+	
+	/*
+	 * Returns the data type, this method allows the logical field to determine
+	 * what kind of physical fields we're gonna need to store the data for this
+	 * type.
+	 */
 	public function getDataType() {
 		return Field::TYPE_CHILDREN;
 	}
 	
+	/**
+	 * This method used to provide a mechanism to finding the target. Since 
+	 * getReferencedField does the job and does it better, this method is deprecated.
+	 * 
+	 * @deprecated since version 0.1-dev 20160905
+	 * @return \Reference
+	 */
 	public function findReference() {
 		$model = $this->getTarget();
 		
