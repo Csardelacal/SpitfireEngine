0712b8ecaaba44548dcfefbc512a4f24758ebfa0
César de la Cal <cesar@magic3w.com>
2017-04-03 21:59:29 +0200

    Adding collections

--- core/CollectionInterface.php
+++ core/CollectionInterface.php
@@ -1,5 +1,32 @@
 <?php namespace spitfire\core;
 
+use Iterator;
+use spitfire\exceptions\core\UndefinedCollectionException;
+
+/* 
+ * The MIT License
+ *
+ * Copyright 2017 César de la Cal Bretschneider <cesar@magic3w.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * A collection is a set of values that can be iterated over and can apply certain
  * operations to it's values.
@@ -12,7 +39,7 @@
  * contains via the toArray() method. It is possible though that this method
  * throws an exception to indicate that the data cannot be casted to array.
  */
-interface CollectionInterface extends \Iterator
+interface CollectionInterface extends Iterator
 {
 	
 	/**
@@ -73,7 +100,7 @@ interface CollectionInterface extends \Iterator
 	 * the exception generated.
 	 * 
 	 * @return mixed
-	 * @throws Exception If the collection cannot be casted to array
+	 * @throws UndefinedCollectionException If the collection cannot be casted to array
 	 * @todo   Name the exception thrown.
 	 */
 	function toArray();
