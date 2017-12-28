753b87b7373a21df9d7b33d9761a3cb57277d5fb
César de la Cal <flunsidelacal@msn.com>
2017-12-28 16:52:14 +0100

    The session looks like a potential bottleneck to me. For some reason I don't trst the old implementation, gonna check how performance of the new code is in production

--- io/session/FileSessionHandler.php
+++ io/session/FileSessionHandler.php
@@ -1,5 +1,7 @@
 <?php namespace spitfire\io\session;
 
+use spitfire\exceptions\FileNotFoundException;
+use spitfire\exceptions\PrivateException;
 use spitfire\io\session\Session;
 
 class FileSessionHandler extends SessionHandler
@@ -8,6 +10,8 @@ class FileSessionHandler extends SessionHandler
 	private $directory;
 	
 	private $handle;
+	
+	private $locked = false;
 
 	public function __construct($directory, $timeout = null) {
 		$this->directory = $directory;
@@ -16,6 +20,7 @@ class FileSessionHandler extends SessionHandler
 
 	public function close() {
 		flock($this->handle, LOCK_UN);
+		fclose($this->handle);
 		return true;
 	}
 
@@ -44,27 +49,33 @@ class FileSessionHandler extends SessionHandler
 		}
 
 		if (!is_dir($this->directory) && !mkdir($this->directory, 0777, true)) {
-			throw new \spitfire\exceptions\FileNotFoundException($this->directory . 'does not exist and could not be created');
+			throw new FileNotFoundException($this->directory . 'does not exist and could not be created');
 		}
+		
+		#Initialize the session itself
+		$id   = Session::sessionId(false);
+		$file = sprintf('%s/sess_%s', $this->directory, $id);
+		
+		$this->handle = fopen($file, 'r+');
+		flock($this->handle, LOCK_EX);
 
 		return true;
 	}
 
 	public function read($__garbage) {
-		$id = Session::sessionId(false);
-		$file = sprintf('%s/sess_%s', $this->directory, $id);
-
-		if (!file_exists($file)) { return ''; }
-		
-		$this->handle = fopen($file, 'r');
-		flock($this->handle, LOCK_EX);
-		
-		return (string)file_get_contents($file);
+		//The system can only read the first 8MB of the session.
+		//We do hardcode to improve the performance since PHP will stop at EOF
+		return (string) fread($this->handle, 8 * 1024 * 1024); 
 	}
 
 	public function write($__garbage, $data) {
-		$id = Session::sessionId(false);
-		return file_put_contents(sprintf('%s/sess_%s', rtrim($this->directory, '\/'), $id), $data) !== false;
+		//If your session contains more than 8MB of data you're probably doing
+		//something wrong.
+		if (isset($data[8*1024*1024])) { 
+			throw new PrivateException('Session length overflow', 171228); 
+		}
+		
+		return fwrite($this->handle, $data);
 	}
 
 }
