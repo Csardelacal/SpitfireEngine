d8a0e130bb53127a366fcd9987610f1b74980185
cesar <cesar@magic3w.com>
2021-08-02 11:53:16 +0200

    Stripped old routing from the controller
    Test Plan: Just removed some code that is no longer needed.
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D875

--- mvc/Controller.php
+++ mvc/Controller.php
@@ -1,8 +1,21 @@
 <?php namespace spitfire\mvc;
 
-use spitfire\core\Context;
-use spitfire\mvc\MVC;
-
+/**
+ * This class just ensures that applications have controllers that are explicitly 
+ * designed to be served publicly. If your controller does not extend this class,
+ * Spitfire will deny access to any requests headed it's way.
+ * 
+ * While this is not technically required with the new Routers, it provides a good
+ * level of enforcement to explicitly declare publicly served content, since
+ * otherwise.
+ * 
+ * Currently, the controllers provide no methods, this means that your application
+ * can use any methods for itself. Future revisions may include a property that 
+ * provides utils for your controller.
+ * 
+ * Please note that you should avoid routing any requests to methods starting with 
+ * an underscore (except for the magic __invoke method)
+ */
 abstract class Controller
 {
 	
