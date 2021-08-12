9f2c402873e0a557780b3c2c6cd158ff3012b4ed
César de la Cal Bretschneider <cesar@magic3w.com>
2021-08-12 12:28:35 +0200

    Rewrote the querytable
    Test Plan:
    There's aminor issue with a return type in `getfields`. Should be fixed on the
    table side
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D887

--- src/QueryTable.php
+++ src/QueryTable.php
@@ -1,9 +1,30 @@
-<?php
+<?php namespace spitfire\storage\database;
 
-namespace spitfire\storage\database;
+use spitfire\collection\Collection;
 
-abstract class QueryTable
+/**
+ * The query table wraps a table and provides a consistent aliasing mechanism.
+ * This allows the system to reference tables within the database system across
+ * queries.
+ * 
+ * For example, when performing a query that requires a table to be joined twice,
+ * the application needs to consistently alias the fields in the query. In SQL
+ * we usually write something like
+ * 
+ * SELECT * FROM orders LEFT JOIN customers c1 ON (...) LEFT JOIN customers c2 ON (...)
+ * 
+ * And then reference the fields within them as c1.id or c2.id. Otherwise, the DBMS
+ * will fail, indicating that the field `id` is ambiguous.
+ */
+class QueryTable
 {
+	
+	/**
+	 * This table provides all the information (metadata and fields) about the table
+	 * being queried.
+	 * 
+	 * @var Table
+	 */
 	private $table;
 	
 	/**
@@ -19,37 +40,87 @@ abstract class QueryTable
 	 * @var int
 	 */
 	private static $counter = 1;
+	
+	/**
+	 * The id is used internally, and is assigned by incrementing the static counter. This
+	 * means that every query table will have a unique id that makes it consistent when disambiguating
+	 * tables.
+	 * 
+	 * @var int
+	 */
 	private $id;
+	
+	/**
+	 * The aliased flag indicates whether the driver should alias this table. This makes
+	 * the generated SQL more readable when debugging.
+	 * 
+	 * @var bool
+	 */
 	private $aliased = false;
 	
-	public function __construct(Table$table) {
+	/**
+	 * @param Table $table
+	 */
+	public function __construct(Table $table) 
+	{
 		#In case this table is aliased, the unique alias will be generated using this.
 		$this->id = self::$counter++;
-		
 		$this->table = $table;
 	}
 	
-	public function getId() {
+	/**
+	 * Get the ID for this table. This is used to generate the table's alias.
+	 * 
+	 * @return int
+	 */
+	public function getId() : int
+	{
 		return $this->id;
 	}
 	
-	public function setId($id) {
-		$this->id = $id;
+	/**
+	 * Creates a copy of this query table, generating a new ID in the process. This is
+	 * due to the fact that these aliases are intended to be immutable.
+	 * 
+	 * @return QueryTable
+	 */
+	public function withNewId() : QueryTable
+	{
+		return new QueryTable($this->table);
 	}
 	
-	public function newId() {
-		$this->id = self::$counter++;
-	}
-	
-	public function setAliased($aliased) {
+	/**
+	 * Determines whether this query table is handled as aliased.
+	 * 
+	 * @param bool $aliased
+	 * @return QueryTable
+	 */
+	public function setAliased(bool $aliased) : QueryTable
+	{
 		$this->aliased = $aliased;
+		return $this;
 	}
 	
-	public function isAliased() {
+	/**
+	 * Indicates whether this table has been aliased. If this is the case, most DBMS
+	 * will need to change the definition of the table when querying.
+	 * 
+	 * @return bool
+	 */
+	public function isAliased() : bool
+	{
 		return $this->aliased;
 	}
 	
-	public function getAlias() {
+	/**
+	 * Retrieves the table's alias. Please note that if the table is set to not alias,
+	 * the system will return the table name. This quirk makes the method rather convenient
+	 * to use.
+	 * 
+	 * @return string
+	 */
+	public function getAlias() : string
+	{
 		/*
 		 * Get the name for the table. We use it to provide a consistent naming
 		 * system that makes it easier for debugging.
@@ -59,30 +130,36 @@ abstract class QueryTable
 		return $this->aliased? sprintf('%s_%s', $name, $this->id) : $name;
 	}
 	
-	public function getField($name) {
-		$of = $this->table->getDb()->getObjectFactory();
-		return $of->queryFieldInstance($this, $this->table->getLayout()->getField($name));
-	}
-	
-	public function getFields() {
-		$of = $this->table->getDb()->getObjectFactory();
-		$fields = $this->table->getLayout()->getFields();
-		
-		foreach ($fields as &$field) {
-			$field = $of->queryFieldInstance($this, $field);
-		}
-		
-		return $fields;
+	/**
+	 * Gets a queryfield.
+	 * 
+	 * @param string $name
+	 * @return QueryField
+	 */
+	public function getField(string $name) : QueryField
+	{
+		return new QueryField($this, $this->table->getLayout()->getField($name));
 	}
 	
 	/**
 	 * 
-	 * @return \spitfire\storage\database\Table
+	 * @return Collection
+	 */
+	public function getFields() : Collection
+	{
+		$fields = $this->table->getLayout()->getFields();
+		
+		return $fields->each(function ($e) {
+			return new QueryField($this, $e);
+		});
+	}
+	
+	/**
+	 * 
+	 * @return Table
 	 */
 	public function getTable() {
 		return $this->table;
 	}
 	
-	abstract public function definition();
-	abstract public function __toString();
 }
