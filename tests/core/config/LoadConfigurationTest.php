6b2d813e0746e0897da385c91568293c0aca6c5e
cesar <cesar@magic3w.com>
2022-05-18 21:41:26 +0200

    The config loader now properly trims the extension off filenames

---
+++ tests/core/config/LoadConfigurationTest.php
@@ -0,0 +1,21 @@
+<?php namespace tests\spitfire\core\config;
+
+use PHPUnit\Framework\TestCase;
+use spitfire\_init\LoadConfiguration;
+use spitfire\core\config\ConfigurationLoader;
+use spitfire\core\Locations;
+
+class LoadConfigurationTest extends TestCase
+{
+	
+	public function testLoad()
+	{
+		$locations = new Locations(__DIR__ . '/_loadFixtures');
+		$loader = new ConfigurationLoader($locations);
+		
+		$config = $loader->make();
+		
+		$this->assertEquals(false, $config->get('app.php.test', false));
+		$this->assertEquals("hello world", $config->get('app.test'));
+	}
+}
