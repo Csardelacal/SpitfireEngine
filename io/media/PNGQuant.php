10482773f5eb8dee2abaaa7df5eefd95e0d41361
cesar <cesar@magic3w.com>
2022-09-09 09:28:55 +0200

    PNGQuant now listens for the exit code
    This should make the process more resilient and help the application to
    be more verbose when failing.

--- io/media/PNGQuant.php
+++ io/media/PNGQuant.php
@@ -6,18 +6,19 @@ class PNGQuant
 {
 	
 	/**
-	 * The PNGQuant function will automatically compress a PNG image, taking just the 
+	 * The PNGQuant function will automatically compress a PNG image, taking just the
 	 * path as a parameter. Since it will write the file to the exact same location
 	 * that your original file was located, you don't need to do any additional work
 	 * to write it.
-	 * 
+	 *
 	 * Usually we don't compress originals, to maintain a "as good as possible" copy,
 	 * but apply this to thumbs and versions we generated with the rather crummy GD
-	 * compression, so you can optionally pass a second parameter to write to a 
+	 * compression, so you can optionally pass a second parameter to write to a
 	 * different file.
-	 * 
+	 *
 	 * @param $img    string The file to read in
 	 * @param $target string The file to write to
+	 * @throws ApplicationException
 	 */
 	public static function compress($img, $target = null)
 	{
@@ -28,22 +29,30 @@ class PNGQuant
 		$descriptors = array(array('pipe', 'r'), array('pipe', 'w'), array('pipe', 'w'));
 		$proc = proc_open('pngquant -', $descriptors, $pipes);
 		
-		if (is_resource($proc)) {
-			fwrite($pipes[0], file_get_contents($img));
-			fclose($pipes[0]);
-			
-			$compressed_png_content = stream_get_contents($pipes[1]);
-			fclose($pipes[1]);
-			
-			$error_output = stream_get_contents($pipes[2]);
-			fclose($pipes[2]);
-			
-			$code = proc_close($proc); //Not yet being used, this is just a test
-		}
-		else {
+		if (!is_resource($proc)) {
 			throw new ApplicationException('Could not initialize PNGQuant process');
 		}
 		
+		fwrite($pipes[0], file_get_contents($img));
+		fclose($pipes[0]);
+		
+		$compressed_png_content = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+		
+		$error_output = stream_get_contents($pipes[2]);
+		fclose($pipes[2]);
+		
+		$code = proc_close($proc);
+		
+		if ($code) {
+			throw new ApplicationException(sprintf('PNGQuant: %s', $error_output));
+		}
+		
+		/**
+		 * Sanity check: If the content is empty, the server was unable to invoke PNGQuant and
+		 * compress the image. This condition should generally be impossible, since we check whether
+		 * PNGQuant is available and whether it ran without error.
+		 */
 		if (!$compressed_png_content) {
 			throw new ApplicationException('Compressing PNG failed. Is pngquant 1.8+ installed on the server?');
 		}
