3fb012cdfba884f23252f47c4248c0ac0f893b31
cesar <cesar@magic3w.com>
2022-09-09 14:52:46 +0200

    Refactored schema diff director
    The execute method was getting a bit bulky and extracting the main logic
    to a separate function should make it easier to understand

--- model/directors/SchemaDiffDirector.php
+++ model/directors/SchemaDiffDirector.php
@@ -5,6 +5,7 @@ use spitfire\collection\Collection;
 use spitfire\model\Model;
 use spitfire\model\utils\AttributeLayoutGenerator;
 use spitfire\storage\database\diff\Generator;
+use spitfire\storage\database\LayoutInterface;
 use spitfire\storage\database\Schema;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -60,44 +61,51 @@ class SchemaDiffDirector extends Command
 		
 		foreach ($models as /** @var ReflectionClass */$model) {
 			$layout = (new AttributeLayoutGenerator())->make($model);
-			
-			if ($this->schema->hasLayoutByName($layout->getTableName())) {
-				$baseline = $this->schema->getLayoutByName($layout->getTableName());
-				$diff = (new Generator($baseline, $layout))->make();
-				
-				foreach ($diff->left()->getFields() as $field) {
-					$output->writeln('<bg=green>Add field</>');
-					$output->writeln(sprintf(' %s', $field->getName()));
-					$output->writeln(sprintf(' %s', $field->getType()));
-					$output->writeln(sprintf(' %s', $field->isNullable()? 'Nullable' : 'Not null'));
-				}
-				
-				foreach ($diff->right()->getFields() as $field) {
-					$output->writeln(sprintf('<bg=red>Remove field</>'));
-					$output->writeln(sprintf(' %s', $field->getName()));
-					$output->writeln(sprintf(' %s', $field->getType()));
-					$output->writeln(sprintf(' %s', $field->isNullable()? 'Nullable' : 'Not null'));
-				}
-				
-				foreach ($diff->left()->getIndexes() as $index) {
-					$output->writeln('<bg=green>Add index</>');
-					$output->writeln(sprintf(' %s', $index->getName()));
-					$output->writeln(sprintf(' %s', $index->getFields()->each(fn($e) => $e->getName())->join(',')));
-				}
-				
-				foreach ($diff->right()->getIndexes() as $index) {
-					$output->writeln('<bg=red>Remove index</>');
-					$output->writeln(sprintf(' %s', $index->getName()));
-					$output->writeln(sprintf(' %s', $index->getFields()->each(fn($e) => $e->getName())->join(',')));
-				}
-			}
-			else {
-				$output->writeln('Missing layout for model ' . $model->getName());
-			}
+			$this->upgrade($output, $layout);
 		}
 		
 		
 		
 		return 0;
+	}
+	
+	private function upgrade(OutputInterface $output, LayoutInterface $layout) : void
+	{
+		/**
+		 * If the layout does not exist at all, we can skip it, since it's just missing.
+		 */
+		if (!$this->schema->hasLayoutByName($layout->getTableName())) {
+			$output->writeln('Missing layout for model ' . $layout->getTableName());
+			return;
+		}
+		
+		$baseline = $this->schema->getLayoutByName($layout->getTableName());
+		$diff = (new Generator($baseline, $layout))->make();
+		
+		foreach ($diff->left()->getFields() as $field) {
+			$output->writeln('<bg=green>Add field</>');
+			$output->writeln(sprintf(' %s', $field->getName()));
+			$output->writeln(sprintf(' %s', $field->getType()));
+			$output->writeln(sprintf(' %s', $field->isNullable()? 'Nullable' : 'Not null'));
+		}
+		
+		foreach ($diff->right()->getFields() as $field) {
+			$output->writeln(sprintf('<bg=red>Remove field</>'));
+			$output->writeln(sprintf(' %s', $field->getName()));
+			$output->writeln(sprintf(' %s', $field->getType()));
+			$output->writeln(sprintf(' %s', $field->isNullable()? 'Nullable' : 'Not null'));
+		}
+		
+		foreach ($diff->left()->getIndexes() as $index) {
+			$output->writeln('<bg=green>Add index</>');
+			$output->writeln(sprintf(' %s', $index->getName()));
+			$output->writeln(sprintf(' %s', $index->getFields()->each(fn($e) => $e->getName())->join(',')));
+		}
+		
+		foreach ($diff->right()->getIndexes() as $index) {
+			$output->writeln('<bg=red>Remove index</>');
+			$output->writeln(sprintf(' %s', $index->getName()));
+			$output->writeln(sprintf(' %s', $index->getFields()->each(fn($e) => $e->getName())->join(',')));
+		}
 	}
 }
