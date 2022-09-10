4a823325beb77e8918d5ef3789d34d048c73838d
cesar <cesar@magic3w.com>
2022-09-10 12:12:49 +0200

    Refactored reflection parameter transformer
    Basically the function was performing the setup, and teardown within
    it's own body. It made sense to break it into a few smaller functions
    that all name what they're doing

--- model/support/ReflectionParameterTransformer.php
+++ model/support/ReflectionParameterTransformer.php
@@ -3,6 +3,7 @@
 use ReflectionClass;
 use ReflectionFunctionAbstract;
 use ReflectionNamedType;
+use ReflectionType;
 use spitfire\exceptions\user\ApplicationException;
 use spitfire\model\Model;
 
@@ -56,67 +57,94 @@ class ReflectionParameterTransformer
 			}
 			
 			/**
-			 * If no type was specified at all for our incoming parameter, we cannot
-			 * resolve it and will use the data we received without editing.
+			 * Make sure the type we're trying to work with is a subtype of model. Otherwise, the application
+			 * cannot resolve it.
 			 */
-			if ($_e === null) {
+			if (!self::isSubtypeOf($_e, Model::class)) {
 				$_return[$_n] = $param;
 				continue;
 			}
 			
-			/**
-			 * If the type is not named, we do not care for it, since we can't work with
-			 * it either.
-			 */
-			if (!($_e instanceof ReflectionNamedType)) {
-				$_return[$_n] = $param;
-				continue;
-			}
-			
-			if ($_e->isBuiltin()) {
-				$_return[$_n] = $param;
-				continue;
-			}
+			assert($_e instanceof ReflectionNamedType);
 			
 			/**
-			 * For our next trick, we're going to need a reflection of the class that the
-			 * developer is expecting us to provide, otherwise we cannot tell if it is a
-			 * subclass of model.
+			 * Make the model and push it onto the parameters.
 			 */
-			$_type = $_e->getName();
-			$class = new ReflectionClass($_type);
-			
-			/**
-			 * Lastly, if the class we expect is not a subtype of model, we also ignore it,
-			 * since the dependency provider will take care of this one.
-			 */
-			if (!$class->isSubclassOf(Model::class)) {
-				$_return[$_n] = $param;
-				continue;
-			}
-			
-			/**
-			 * Fetch the database record that we expected from the database.
-			 */
-			$model = db()->fetch($_e->getName(), $param);
-			
-			/**
-			 * Perform a sanity check in the development environment to ensure the consistency
-			 * of our data.
-			 */
-			assert($model === null || $model instanceof $_type);
-			
-			/**
-			 * As a safeguard, we ensure that if the method we're trying to build parameters for
-			 * accepts no nullable type for this we throw an exception if our query came up empty.
-			 */
-			if ($model === null && !$_e->allowsNull()) {
-				throw new ApplicationException('Could not reesolve the model for a parameter that does not accept null', 2111161127);
-			}
-			
-			$_return[$_n] = $model;
+			$_return[$_n] = self::make($_e, $param);
 		}
 		
 		return $_return;
+	}
+	
+	private static function isSubtypeOf(?ReflectionType $_e, string $classname) : bool
+	{
+		
+		/**
+		 * If no type was specified at all for our incoming parameter, we cannot
+		 * resolve it and will use the data we received without editing.
+		 */
+		if ($_e === null) {
+			return false;
+		}
+		
+		/**
+		 * If the type is not named, we do not care for it, since we can't work with
+		 * it either.
+		 */
+		if (!($_e instanceof ReflectionNamedType)) {
+			return false;
+		}
+		
+		if ($_e->isBuiltin()) {
+			return false;
+		}
+		
+		/**
+		 * For our next trick, we're going to need a reflection of the class that the
+		 * developer is expecting us to provide, otherwise we cannot tell if it is a
+		 * subclass of model.
+		 */
+		$_type = $_e->getName();
+		$class = new ReflectionClass($_type);
+		
+		/**
+		 * Lastly, if the class we expect is not a subtype of model, we also ignore it,
+		 * since the dependency provider will take care of this one.
+		 */
+		if (!$class->isSubclassOf($classname)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static function accepts(ReflectionNamedType $_e, $model) : bool
+	{
+		$_type = $_e->getName();
+		return $model instanceof $_type;
+	}
+	
+	private static function make(ReflectionNamedType $_e, $param) : Model
+	{
+		/**
+		 * Fetch the database record that we expected from the database.
+		 */
+		$model = db()->fetch($_e->getName(), $param);
+		
+		/**
+		 * Perform a sanity check in the development environment to ensure the consistency
+		 * of our data.
+		 */
+		assert($model === null || self::accepts($_e, $model));
+		
+		/**
+		 * As a safeguard, we ensure that if the method we're trying to build parameters for
+		 * accepts no nullable type for this we throw an exception if our query came up empty.
+		 */
+		if ($model === null && !$_e->allowsNull()) {
+			throw new ApplicationException('Could not resolve the model for a parameter that does not accept null', 2111161127);
+		}
+		
+		return $model;
 	}
 }
