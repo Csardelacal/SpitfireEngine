d9861758e2740af0f46d36197ae8b5aecc9fd382
cesar <cesar@magic3w.com>
2022-11-02 09:21:07 +0100

    Added return values to string operations
    Now the applications working with them should know better what they're
    actually working with. Strict types baby!

--- utils/Strings.php
+++ utils/Strings.php
@@ -25,7 +25,7 @@ class Strings
 	 *                      "CamelCase" (true) or "camelCase" (false)
 	 * @return string
 	 */
-	public static function underscores2camel($str, $high = true)
+	public static function underscores2camel($str, $high = true) : string
 	{
 		trigger_error("The function underscores2camel was renamed to camel", E_USER_DEPRECATED);
 		return self::camel($str, $high);
@@ -49,13 +49,13 @@ class Strings
 		return $high? ucfirst($_ret) : $_ret;
 	}
 	
-	public static function ellipsis($str, $targetlength, $char = '…')
+	public static function ellipsis(string $str, int $targetlength, string $char = '…') : string
 	{
-		$newlen = $targetlength - strlen($char);
-		return (strlen($str) > $newlen)? substr($str, 0, $newlen) . '…' : $str;
+		$newlen = $targetlength - mb_strlen($char);
+		return (mb_strlen($str) > $newlen)? mb_substr($str, 0, $newlen) . $char : $str;
 	}
 	
-	public static function slug($string)
+	public static function slug(string $string) : string
 	{
 		
 		$str = strtolower(preg_replace(
@@ -64,15 +64,17 @@ class Strings
 			$string
 		));
 		
+		/**
+		 * @see http://stackoverflow.com/questions/10444885/php-replace-foreign-characters-in-a-string
+		 */
 		return preg_replace(
-				  /*http://stackoverflow.com/questions/10444885/php-replace-foreign-characters-in-a-string*/
 			'/&([A-Za-z]{1,2})(acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);/i',
 			'$1', //Remove accents
 			htmlentities($str, ENT_QUOTES, 'UTF-8')
 		);
 	}
 	
-	public static function endsWith($haystack, $needle)
+	public static function endsWith(string $haystack, string $needle) : bool
 	{
 		if (!$needle) {
 			return true;
@@ -80,7 +82,7 @@ class Strings
 		return strcmp(substr($haystack, 0 - strlen($needle)), $needle) === 0;
 	}
 	
-	public static function rTrimString(string $haystack, string $needle)
+	public static function rTrimString(string $haystack, string $needle) : string
 	{
 		if (self::endsWith($haystack, $needle)) {
 			return substr($haystack, 0, 0 - strlen($needle));
@@ -89,7 +91,7 @@ class Strings
 		return $haystack;
 	}
 	
-	public static function startsWith($haystack, $needle)
+	public static function startsWith(string $haystack, string $needle) : bool
 	{
 		if (empty($needle)) {
 			return true;
@@ -97,7 +99,7 @@ class Strings
 		return strpos($haystack, $needle) === 0;
 	}
 	
-	public static function plural($string)
+	public static function plural(string $string) : string
 	{
 		if (Strings::endsWith($string, 'y')) {
 			return substr($string, 0, -1) .'ies';
@@ -106,7 +108,7 @@ class Strings
 		}
 	}
 	
-	public static function singular($string)
+	public static function singular(string $string) : string
 	{
 		if (Strings::endsWith($string, 'ies')) {
 			return substr($string, 0, -3) .'y';
@@ -128,7 +130,7 @@ class Strings
 		return Strings::urls($str);
 	}
 	
-	public static function urls($str, $cb = null)
+	public static function urls(string $str, callable $cb = null) : string
 	{
 		$urlRegex = '#(https?://[a-zA-z0-9%&?/.\-_=+;@\#]+)#';
 		$flip = false;
@@ -148,7 +150,7 @@ class Strings
 		})->join('');
 	}
 	
-	public static function quote($str)
+	public static function quote(string $str) : string
 	{
 		return str_replace(['\'', '"'], ['&#039;', '&quot;'], $str);
 	}
