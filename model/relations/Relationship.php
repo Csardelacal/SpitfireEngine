f56186de900f14c1fbb9ac21b98a038121c16a54
César de la Cal Bretschneider <cesar@magic3w.com>
2023-01-19 20:24:29 +0100

    Relationships act as querybuilders but they're not
    This change reduces the codebase of the class significantly, and removes
    the requirement from the relationship to implement all the methods. We
    rely on the mixins to implement them properly

--- model/relations/Relationship.php
+++ model/relations/Relationship.php
@@ -5,7 +5,6 @@ use spitfire\model\Field;
 use spitfire\model\Model;
 use spitfire\model\query\RestrictionGroupBuilder;
 use spitfire\model\QueryBuilder;
-use spitfire\model\QueryBuilderInterface;
 use spitfire\storage\database\Query;
 use spitfire\storage\database\query\RestrictionGroup;
 use spitfire\utils\Mixin;
@@ -17,28 +16,28 @@ use spitfire\utils\Mixin;
  * @implements RelationshipInterface<LOCAL,REMOTE>
  * @mixin QueryBuilder<REMOTE>
  */
-abstract class Relationship implements RelationshipInterface, QueryBuilderInterface
+abstract class Relationship implements RelationshipInterface
 {
 	
 	use Mixin;
 	
 	/**
-	 * 
+	 *
 	 * @var Field<LOCAL>
 	 */
 	private Field $field;
 	
 	/**
-	 * 
+	 *
 	 * @var Field<REMOTE>
 	 */
 	private Field $referenced;
 	
 	/**
-	 * 
+	 *
 	 * @param Field<LOCAL> $field
 	 * @param Field<REMOTE> $referenced
-	 * 
+	 *
 	 */
 	public function __construct(Field $field, Field $referenced)
 	{
@@ -58,7 +57,7 @@ abstract class Relationship implements RelationshipInterface, QueryBuilderInterf
 	}
 	
 	/**
-	 * 
+	 *
 	 * @return Field<LOCAL>
 	 */
 	public function getField() : Field
@@ -67,7 +66,7 @@ abstract class Relationship implements RelationshipInterface, QueryBuilderInterf
 	}
 	
 	/**
-	 * 
+	 *
 	 * @return Field<LOCAL>
 	 */
 	public function localField() : Field
@@ -76,7 +75,7 @@ abstract class Relationship implements RelationshipInterface, QueryBuilderInterf
 	}
 	
 	/**
-	 * 
+	 *
 	 * @return Field<REMOTE>
 	 */
 	public function getReferenced() : Field
@@ -85,68 +84,10 @@ abstract class Relationship implements RelationshipInterface, QueryBuilderInterf
 	}
 	
 	/**
-	 * 
+	 *
 	 * @return QueryBuilder<REMOTE>
 	 */
 	abstract public function startQueryBuilder(): QueryBuilder;
 	
 	abstract public function injector() : RelationshipInjectorInterface;
-	
-	# The following functions are used to provide query building. Since PHP won't let us
-	# just __call all our interface methods, we need to manually create the mixin.
-	# TODO: Maybe a more elegant solution for this would be good
-	# TODO: Add docblocks for these methods.
-	public function all() : Collection
-	{
-		return $this->startQueryBuilder()->all();
-	}
-	
-	public function count(): int
-	{
-		return $this->startQueryBuilder()->count();
-	}
-	
-	public function first(?callable $or = null): ?Model
-	{
-		return $this->startQueryBuilder()->first($or);
-	}
-	
-	public function getQuery(): Query
-	{
-		return $this->startQueryBuilder()->getQuery();
-	}
-	
-	public function getRestrictions(): RestrictionGroupBuilder
-	{
-		return $this->startQueryBuilder()->getRestrictions();
-	}
-	
-	/**
-	 * 
-	 * @param RestrictionGroup::TYPE_* $type
-	 * @param callable $do
-	 * @return QueryBuilder<REMOTE>
-	 */
-	public function group(string $type, callable $do): QueryBuilder
-	{
-		return $this->startQueryBuilder()->group($type, $do);
-	}
-	
-	/**
-	 * 
-	 * @return Collection<REMOTE>
-	 */
-	public function range(int $offset, int $size): Collection
-	{
-		return $this->startQueryBuilder()->range($offset, $size);
-	}
-	
-	/**
-	 * 
-	 * @return QueryBuilder<REMOTE>
-	 */
-	public function restrictions(callable $do): QueryBuilder
-	{
-		return $this->startQueryBuilder()->restrictions($do);
-	}
 }
