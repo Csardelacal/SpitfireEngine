d25106e7c1e999fbafc1ad6f130740add1ec8cc0
cesar <cesar@magic3w.com>
2023-04-20 19:43:26 +0200

    Cannot write to undefined vars
    Connection manager was being naughty and writing to variables it had not
    yet defined.

--- src/storage/database/ConnectionManager.php
+++ src/storage/database/ConnectionManager.php
@@ -1,5 +1,26 @@
 <?php namespace spitfire\storage\database;
 
+/*
+ *
+ * Copyright (C) 2023-2023 César de la Cal Bretschneider <cesar@magic3w.com>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-13 01  USA
+ *
+ */
+
 use Psr\Container\ContainerInterface;
 use spitfire\collection\Collection;
 use spitfire\collection\TypedCollection;
@@ -58,7 +79,7 @@ class ConnectionManager
 		}
 		
 		$connection = $this->make($name);
-		$this->connections->$name = $connection;
+		$this->connections->offsetSet($name, $connection);
 		return $connection;
 	}
 	
